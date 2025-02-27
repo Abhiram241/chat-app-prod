e07f0398d6aeabb493933d7f19fe10ba90080a3f
Abhiram <81345491+Abhiram241@users.noreply.github.com>
2025-02-27 14:21:18 +0530

    fixing static file path

--- backend/src/index.js
+++ backend/src/index.js
@@ -76,9 +76,9 @@ app.use("/api/messages", messageRoutes);
 // });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist"))); // Serve static files
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html")); // Serve index.html
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 server.listen(PORT, () => {
